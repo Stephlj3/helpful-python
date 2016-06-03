c955aa732d80c31fe602d4675338d5d83b2742fa
Stephlj3 <steph.johnson@avegant.com>
2016-06-03 11:19:40 -0700

    adding scripts

---
+++ TIBQ to TI_I2CFormat.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 13:53:17 2015
+
+@author: Steph
+
+Takes generated coefficient file from TIBQ.exe and outputs TI I2C format
+
+This script intended for TLV320AIC3256 DAC PRB25 - generates Biquads B through F only
+See pg.44-45 of Applications Resource Guide : http://www.ti.com/lit/ug/slau306a/slau306a.pdf
+
+"""
+import array
+
+d = 5
+j = 32 #left biquad 2 starts at register 32
+k = 40 #right biquad 2 starts at register 40
+a = s = 1
+b = c = e = f =  0
+t = ['w 30 ']
+
+with open('Generated Coefficients_2.txt', 'rb') as in_file:  #Input file
+    with open('Biquad_TI_I2C_Format.txt', 'wb') as out_file:  #output file
+        out_file.write('#WARNING: This code has been generated automatically. Edit at your own risk\n\n')
+
+        for line in in_file:
+                out_file.write('#%s' % (line))
+
+                if 'Filter %d BQ' % (a) in line:
+                    out_file.write('#-----------------------------------------------------------------\n')
+                    out_file.write('#Set Left Biquad %d (start at Register %d)\n' % (a,j))
+                    t.append('%02x ' % (j))
+                    a += 1
+                    j += 20
+                    out_file.write('#select pg. 44 - Left DAC\n')
+                    out_file.write('w 30 00 2C\n\n')          
+                    
+                elif '0x' in line:
+                    b += 1
+                    c = b % 5
+                    for d in xrange(2,8):
+                            t.append('%s' % (line[d]))
+                            e += 1
+                            f += 1
+                            if e % 2 == 0 :
+                                    t.append(' ')
+                                    if f % 3 == 0 and c != 0:
+                                            t.append('00 ')
+
+                    
+                    if '0x' in line and c == 0:
+
+                            #copy into left dac
+                            u = "".join(t)
+                            out_file.write('\n%s\n\n' % (u))
+
+                            #copy into right dac
+                            out_file.write('#Set Right Biquad %d (start at Register %d)\n' % (s,k))
+                            t.pop(1)
+                            t.insert(1, '%02x ' % (k))
+                            s += 1
+                            k += 20
+                            out_file.write('#select pg. 45 - Right DAC\n')
+                            out_file.write('w 30 00 2D\n')
+                            u = "".join(t)
+                            out_file.write('\n%s\n\n' % (u))
+
+                            #reset for next biquad
+                            t = ['w 30 ']
+                            
---
+++ serial read.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 13:53:17 2015
+
+@author: Steph
+
+This script creates a serial port on windows machines "com" at the 
+specified baud rate.  It will loop over a command send to the port 
+and print to a text file the full port response.  It also tracks
+the time between writes.
+
+"""
+
+import datetime
+import time
+import serial
+
+#Open Serial Port  -- update COM# and baud rate as needed
+ser = serial.Serial('COM5', 115200)
+start = time.time()
+
+while ser.isOpen():
+
+    try:
+
+        f = open('your file name.txt', 'a') #Update output file name 
+        ser.write("some text to serial port\r") #data to serial port
+        info = ser.read(ser.inWaiting()) #read from serial port
+        f.write('datetime: %s \n%s \n' % 
+        (datetime.datetime.now(), info)) #write to text file
+        print info
+        time.sleep(10) #sleep until next write
+        end = time.time()
+        elapsed = end - start
+        print elapsed
+
+
+    except Exception, e:
+        print "error: " + str(e)
+        raise
+
+
